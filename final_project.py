29dfc4b202143d029f01bb1c00a4afddd48e1776
marcocolognesi <marcocolognesi5@gmail.com>
2023-02-15 20:41:00 +0100

    started to put data visualization on streamlit

--- final_project.py
+++ final_project.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import seaborn as sns
 
-st.title("EDA and Visualization of global suicide rates from 1986 to 2021")
+st.title("EDA and Visualization of global suicide rates from 1985 to 2020")
 st.markdown(
 '''
 Suicide is a serious global public health issue. It is among the top twenty leading causes of death worldwide, with more deaths due to suicide than to malaria, breast cancer, or war and homicide. 
 Close to 800 000 people die by suicide every year (*World Health Organization, 2019*).
 \n 
-The aim of this project is to analyze and visualize the different suicide rates around the world, from 1986 to 2021.
+The aim of this project is to analyze and visualize the different suicide rates around the world, from 1985 to 2020.
 '''
 )
 
@@ -98,7 +98,7 @@ if st.checkbox('Click to see the code', key='1'):
 st.header('Manipulation of the null values')
 st.markdown(
 '''
-Looking at the info of our dataset, we see that there are some **null values** in the suicide rates column (*precisely 1200 null values*).
+Looking at the info of our dataset, we see that there are some **null values** in the suicide rates column (*precisely 1200 null values, which are only from 2017 to 2020*).
 \n We can now procede in two ways: either **drop** them or **fix** them.
 \n Suicide rates are not an easily predictable number as many factors could influence them. In a certain year there might be a huge increase (*for example due to a big shock in the world economy*) or decrease 
 (*for example because of a relatively stable situation*). The best decision should be to drop the nulls, however, although it may result inaccurate, for the sake of this project we decide to manipulate the values 
@@ -125,7 +125,7 @@ if st.checkbox('Click to see the code', key='2'):
 st.markdown(
 '''
 We have now succesfully created a new dataframe without null values. This move, however, messes up the dataframe a little bit. The suicide rates over 100k population related to the previously null values are still
-equal to 0. Fixing them is not difficult, however, from 2016 to 2021 the population is sometimes not counted properly like in the previous years (*it uses the population of the whole country for each rate instead of
+equal to 0. Fixing them is not difficult, however, from 2016 to 2020 the population is sometimes not counted properly like in the previous years (*it uses the population of the whole country for each rate instead of
 using the population of each group and gender*).
 \n This incongruence is due to the fact that from that year, the data has been update by a different author.
 \n For this reason we are gonna use the new dataset with the estimated rates only in certain plots and a dataset until 2016 for others. 
@@ -135,28 +135,95 @@ using the population of each group and gender*).
 suicide_df_filled = pd.read_csv('fixed_values.csv') #See the colab file for the wall of text of the manipulation
 suicide_df_filled.drop('Unnamed: 0',axis=1, inplace=True)
 
-#========================================================================
+suicide_df_not_filled = suicide_df.dropna()
 
-#Streamlit presentation and Data visualization
+year_2016_mask = suicide_df_not_filled['year'] < 2016
+suicide_df_until_2016 = suicide_df_not_filled[year_2016_mask]
+
+#==========================================================================================================================================================================================================
+
+#DATA VISUALIZATION
+
 st.header('Data Visualization')
 
 #1. Total rates analysis
-st.subheader('1. Total Rates Analysis')
-year_groupby_suicidesum =suicide_df.groupby('year').suicides_no.sum()
-f, ax = plt.subplots(figsize=(16,8))
-sns.barplot(x=year_groupby_suicidesum.values , y= year_groupby_suicidesum.index, color = 'c', orient='h')
-ax.set_title('Total suicide rates from 1986 to 2020', weight='bold')
-#ax.ticklabel_format(axis = 'x', style = 'plain')
-ax.set_ylabel('Year')
-ax.set_xlabel('Total suicide rates')
+st.subheader('Analysis of the total rates from 1985 to 2020')
+
+year_groupby_sumrates_filled = suicide_df_filled.groupby('year').suicides_no.sum()
+
+col_1 , col_2 = st.columns(2)
+with col_1:
+    f, ax = plt.subplots(figsize=(8,6))
+    sns.barplot(x=year_groupby_sumrates_filled.values , y= year_groupby_sumrates_filled.index, color = 'c', orient='h')
+    ax.set_title('Trend of the total suicide rates from 1985 to 2020', weight='bold')
+    #ax.ticklabel_format(axis = 'x', style = 'plain')
+    ax.set_ylabel('Year')
+    ax.set_xlabel('Total suicide rates')
+    st.write(f)
+with col_2:
+    f, ax = plt.subplots(figsize=(8,6))
+    sns.lineplot(x=year_groupby_sumrates_filled.index , y= year_groupby_sumrates_filled.values)
+    ax.set_title('Trend of the total suicide rates from 1985 to 2020', weight='bold')
+    #ax.ticklabel_format(axis = 'x', style = 'plain')
+    ax.set_ylabel('Total suicide rates')
+    ax.set_xlabel('Year')
+    st.write(f)
+
+st.caption('Trend of the total suicide rates from 1985 to 2020 (*using the data with the estimated values obtained with the mean*)')
+st.markdown(
+'''
+Looking at the trend of the total suicide rates from 1985 to 2020, we can see that the peak has occurred in the late 1990's - early 2000's, in particular from 1999 to 2003.
+\n Since then, the trend went down and reached numbers similar to the ones from the early 1990's. Also, the huge decrease in 2016 is due to the fact that we have a lot less data for that year (*only 160 values*).
+\n Now let's see which are the top 10 countries with the most rates over the years, sorting also by gender and age.
+'''
+)
+
+#Finding out which are the top 10 countries with the most suicide rates over the years
+country_groupby_sumrates_filled = suicide_df_filled.groupby('country').suicides_no.sum()
+top_10_countries = country_groupby_sumrates_filled.sort_values(ascending=False)[0:10].index
+top_10_most_suicide_rates = country_groupby_sumrates_filled.sort_values(ascending=False)[0:10].values
+
+state_list = ['Russian Federation', 'United States of America', 'Japan', 'Ukraine', 'France','Germany','Republic of Korea','Brazil', 'Poland', 'United Kingdom']
+labels = ['Russia', 'USA', 'Japan', 'Ukraine', 'France', 'Germany', 'South Korea', 'Brazil', 'Poland', 'UK']
+
+top_10_countries_most_suicides_rates_df_filled = suicide_df_filled.query('country in @state_list')
+
+top_10_countries_gender_groupby_sumrates_filled = top_10_countries_most_suicides_rates_df_filled.groupby(['country','gender']).suicides_no.sum()
+top_10_countries_gender_groupby_sumrates_filled_sorted = top_10_countries_gender_groupby_sumrates_filled.reset_index().set_index('country').loc[state_list]
+
+top_10_countries_age_groupby_sumrates_filled = top_10_countries_most_suicides_rates_df_filled.groupby(['country','age']).suicides_no.sum()
+top_10_countries_age_groupby_sumrates_filled_sorted = top_10_countries_age_groupby_sumrates_filled.reset_index().set_index('country').loc[state_list]
+
+f, ax = plt.subplots(3,1,figsize=(12,10))
+sns.barplot(x= top_10_countries, y= top_10_most_suicide_rates, palette = "rocket", ax=ax[0])
+ax[0].set_title('Top 10 countries with most suicides from 1986 to 2020, gender and age comparison', weight='bold')
+ax[0].ticklabel_format(axis = 'y', style = 'plain')
+ax[0].set_ylabel('')
+ax[0].set_xlabel('')
+ax[0].set_xticklabels(labels)
+#============================
+sns.barplot(x= top_10_countries_gender_groupby_sumrates_filled_sorted.index, y= 'suicides_no', hue='gender', data= top_10_countries_gender_groupby_sumrates_filled_sorted, palette=('hotpink','cornflowerblue'), ax=ax[1])
+ax[1].ticklabel_format(axis = 'y', style = 'plain')
+ax[1].set_ylabel('Total suicide rates')
+ax[1].set_xlabel('')
+ax[1].set_xticklabels(labels)
+#============================
+sns.barplot(x= top_10_countries_age_groupby_sumrates_filled_sorted.index, y= 'suicides_no', hue='age', data= top_10_countries_age_groupby_sumrates_filled_sorted, palette='rocket_r', ax=ax[2])
+ax[2].set_ylabel('')
+ax[2].set_xlabel('Country')
+ax[2].set_xticklabels(labels)
+
 st.write(f)
-
-print(suicide_df.info())
-
-
-#========================================================================
-#Data Visualization 
-
+st.caption('Top 10 countries with the most suicide rates over from 1985 to 2020, using the data with the estimated values obtained with the mean')
+st.markdown(
+'''
+Looking at the plot, we see that the most suicides occurred in Russia and USA. This shouldn't be surprising as these countries are among the largest in the world and with more population than the others.
+Also, we need to take into consideration the fact that we don't have reliable data (*or no data at all*) for a lot of important countries, such as China and India for example, but also for most of the african ones.
+- The **gender comparison** shows us that among these countries we had **more male suicides than female ones**. 
+- Japan seems to be the country with most female suicides.
+- The **age group comparison** shows us that in each of the countries in this list, **most of the rates occurred in the 35-54 age group**. From this comparison we see also that Japan has the most rates within the 15-24 and 55-74 age group. We can also see that Brazil has a strangely high rate in the 15-24 age group, in comparison with the other countries near his position.
+'''
+)
 #2. Gender analysis
 #3. Age group analysis
 #4. Correlation
